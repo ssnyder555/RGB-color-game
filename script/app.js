f8deb295bedae12fa79dd412af462ee3bd312481
ssnyder555 <ssnyder555@live.com>
2018-11-28 17:03:36 -0600

    random color change

--- script/app.js
+++ script/app.js
@@ -8,7 +8,7 @@ let colors = [
 ]
 
 let squares = document.querySelectorAll(".square");
-let pickedColor = colors[3];
+let pickedColor = pickColor();
 let colorDisplay = document.getElementById('colorDisplay');
 let messageDisplay = document.querySelector('#message');
 
@@ -39,4 +39,9 @@ function changeColors(color){
     squares[i].style.background = color;
   }
   // change each color to match given color
+}
+
+function pickColor(){
+  let random = Math.floor(Math.random() * colors.length);
+  return colors[random];
 }
